726ec687a32de78731201547060a60bea8b274c0
Edwin Agbenyega <eddybanks03@gmail.com>
2020-06-15 23:11:53 -0600

    Update stripe integration and update the favicon and title

--- src/components/collections-overview/collections-overview.js
+++ src/components/collections-overview/collections-overview.js
@@ -2,7 +2,7 @@ import React from 'react'
 import CollectionPreview from '../collection-preview/collection-preview'
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
-import { selectCollections } from '../../redux/shop/shop.selectors'
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors'
 import './collections-overview.styles.scss'
 
 const CollectionsOverview = ({ collections }) => (
@@ -18,7 +18,7 @@ const CollectionsOverview = ({ collections }) => (
 );
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections
+  collections: selectCollectionsForPreview
 })
 
 export default connect(mapStateToProps)(CollectionsOverview)
--- src/pages/collection/collection.js
+++ src/pages/collection/collection.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-import './collections.styles.scss'
+import './collection.styles.scss'
 
-// import CollectionItem from '../../components/collection-item/collection-item'
+import CollectionItem from '../../components/collection-item/collection-item'
 import { selectCollection } from '../../redux/shop/shop.selectors'
 
 const CollectionPage = ({ collection }) => {
-  console.log(collection)
+  const { title, items } = collection
   return (
     <div className='collection-page'>
-      <h2>Collections Page</h2>
+      <h2 className='title'>{ title}</h2>
+      <div className='items'>
+        {
+          items.map(item => (
+            <CollectionItem key={item.id} item={item} />
+          ))
+        }
+      </div>
     </div>
   )
 }
